02b05c945795d85a1e4e47e9b88ea6ee13573f67
JLGOrtega <joseluis.gomez@thebridgeschool.es>
2023-09-07 13:41:02 +0200

    a ver

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, send_from_directory, render_template, redirect, request
+from flask import Flask, send_from_directory, render_template, redirect, request, jsonify
 from inutils import sorpresa
+from sqlalchemy import create_engine
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -12,6 +14,11 @@ def hello():
     if "name" in request.args:
         name = request.args["name"]
     return f"<h1>Hello {name}</h1><br>{sorpresa}"
+
+@app.route("/table", methods=["GET"])
+def table():
+    engine = create_engine("postgresql://fl0user:bleB3zxJy9nN@ep-steep-fire-21690954.eu-central-1.aws.neon.tech:5432/postgres?sslmode=require")
+    return jsonify(pd.read_sql_table("test", con=engine).to_dict("records"))
 
 if __name__ == "__main__":
     app.run(port=port)
