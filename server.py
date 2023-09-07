e5f545a84753a9cb2b882f6c4199515fc17029d5
JLGOrtega <joseluis.gomez@thebridgeschool.es>
2023-09-07 13:03:18 +0200

    fffDJFKJ

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
-from flask import Flask, request
+import os
+from flask import Flask, send_from_directory, render_template, redirect, request
 from inutils import sorpresa
 
 app = Flask(__name__)
+
+port = int(os.environ.get("PORT", 5000))
 
 @app.route("/", methods=["GET"])
 def hello():
@@ -10,8 +13,5 @@ def hello():
         name = request.args["name"]
     return f"<h1>Hello {name}</h1><br>{sorpresa}"
 
-
-if __name__ ==  "__main__":
-    app.run(debug=True)
-
-
+if __name__ == "__main__":
+    app.run(port=port)
